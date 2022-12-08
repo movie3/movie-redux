2f4df4c5e1f8befd09ca275813e08a4d27f70448
khalledalkarmi <khalled_95@hotmail.com>
2022-12-08 19:56:39 +0300

    add slider and subscribe pages

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Hero from './Hero'
 import Slider from './Slider'
+import Subscribe from './Subscribe'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Slider />
-      <Slider />
+      <Slider title={'Top Rating'} />
+      <Slider title={'Up Coming '} />
+      <Subscribe />
     </>
   )
 }
--- src/Components/Home/Subscribe.jsx
+++ src/Components/Home/Subscribe.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <div className='grid '>
+      <section class="pricing-section spad">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="col-lg-8 offset-lg-2 text-center">
+                <div class="section-title"> <br /><br />
+                  <h3><span class="orange-text">Choose</span> Your Plan</h3>
+                </div>
+              </div>
+            </div>
+            <div class="container1">
+
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-8">
+              <div class="ps-item">
+                <h3>Class drop-in</h3>
+                <div class="pi-price">
+                  <h2>$ 39.0</h2>
+                  <span>SINGLE CLASS</span>
+                </div>
+                <ul>
+                  <li>Free riding</li>
+                  <li>Unlimited equipments</li>
+                  <li>Personal trainer</li>
+                  <li>Weight losing classes</li>
+                  <li>Month to mouth</li>
+                  <li>No time restriction</li>
+                </ul>
+                <a href="checkout.html" class="primary-btn pricing-btn">Enroll now</a>
+                <a href="checkout.html" class="thumb-icon"><i class="fa fa-picture-o"></i></a>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-8">
+              <div class="ps-item">
+                <h3>12 Month unlimited</h3>
+                <div class="pi-price">
+                  <h2>$ 99.0</h2>
+                  <span>SINGLE CLASS</span>
+                </div>
+                <ul>
+                  <li>Free riding</li>
+                  <li>Unlimited equipments</li>
+                  <li>Personal trainer</li>
+                  <li>Weight losing classes</li>
+                  <li>Month to mouth</li>
+                  <li>No time restriction</li>
+                </ul>
+                <a href="checkout.html" class="primary-btn pricing-btn">Enroll now</a>
+                <a href="checkout.html" class="thumb-icon"><i class="fa fa-picture-o"></i></a>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-8">
+              <div class="ps-item">
+                <h3>6 Month unlimited</h3>
+                <div class="pi-price">
+                  <h2>$ 59.0</h2>
+                  <span>SINGLE CLASS</span>
+                </div>
+                <ul>
+                  <li>Free riding</li>
+                  <li>Unlimited equipments</li>
+                  <li>Personal trainer</li>
+                  <li>Weight losing classes</li>
+                  <li>Month to mouth</li>
+                  <li>No time restriction</li>
+                </ul>
+                <a href="checkout.html" class="primary-btn pricing-btn">Enroll now</a>
+                <a href="checkout.html" class="thumb-icon"><i class="fa fa-picture-o"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
