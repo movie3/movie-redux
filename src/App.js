c8dadaeeae2135597ce6239e7711007ac7bce82e
khalledalkarmi <khalled_95@hotmail.com>
2022-12-10 03:00:39 +0300

    add sideBar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import { Footer, Header, Home } from "./Components";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
+import Profile from "./Components/Profile";
 
 
 function App() {
   return (
     <>
-      <div className="bg-main text-white ">
         <Header />
+      <div className="bg-main text-white ">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/Components/Profile/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const Profile = () => {
+    return (
+        <SideBar />
+    )
+}
+
+export default Profile
