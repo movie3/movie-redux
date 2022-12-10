e4249b0ab8c9f335f0efca357235e307462d79bf
aalawneh220 <ahmadalawneh329@gmail.com>
2022-12-10 20:47:54 +0300

    login

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 import { FiLogIn } from "react-icons/fi";
+import { MdEmail, MdPassword } from "react-icons/md";
 import { Link } from "react-router-dom";
 // import { Input } from "../Main-Component/UsedInputs";
+import img from "../img/bg-c.jpg";
 import {
   TextInput,
   PasswordInput,
@@ -14,46 +16,49 @@ import {
   Checkbox,
   Anchor,
   Stack,
-} from '@mantine/core';
-import { Input } from '@mantine/core';
-import { IconAt,IconAsterisk } from '@tabler/icons';
-import { GoogleIcon } from './GoogleIcon'
+} from "@mantine/core";
+import { Input } from "@mantine/core";
+import { IconAt, IconAsterisk } from "@tabler/icons";
+import { GoogleIcon } from "./GoogleIcon";
 // import Logo from '../asset/movie-logo.png'
 const Login = () => {
   //TODO: Age is roznameh
   return (
-    <div className="container mx-auto pt-10 flex-colo min-h-[72vh]">
-      <div className="w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:w-3/5 bg-dry  rounded-lg border border-border">
+    <div
+      className=" w-100 pt-10 flex-colo min-h-[95vh] bg-cover backdrop-blur-lg "
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div className="w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:w-3/5   rounded-lg border border-border text-center ml-10  mt-8">
         <Group grow mb="md" mt="md">
           <GoogleIcon radius="xl">Google</GoogleIcon>
           {/* <TwitterButton radius="xl">Twitter</TwitterButton> */}
         </Group>
 
-        <Divider label="Or continue with email" labelPosition="center" my="lg" />
-        <p className='text-white text-center text-3xl'>Movie Logo</p>
-        <Input
-          className='my-4'
-          icon={<IconAt />}
-          placeholder="Your email"
+        <Divider
+          label="Or continue with email"
+          labelPosition="center"
+          my="lg"
         />
-        <PasswordInput
-          icon={<IconAsterisk />}
-          placeholder="Your email"
-        />
-        <div className='flex justify-center'>
-          <Button className='bg-subMain transitions hover:bg-main rounded-lg w-1/2 my-3' rightIcon={<FiLogIn />}>Login</Button>
+        <p className="text-white text-center text-3xl">Movie Logo</p>
+        <Input className="my-4" icon={<MdEmail />} placeholder="Your email" />
+        <PasswordInput icon={<MdPassword />} placeholder="Your Password" />
+        <div className="flex justify-center">
+          <Button
+            className="bg-subMain transitions hover:bg-main rounded-lg w-1/2 my-3"
+            rightIcon={<FiLogIn />}
+          >
+            Login
+          </Button>
         </div>
         <p className="text-center text-border">
           Don't have an account?{" "}
-
           <Link to="/register" className="text-dryGray font-semibold ml-2">
             Sign Up
           </Link>
         </p>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
