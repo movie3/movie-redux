d54efd65ba26cc595e4b1d93883efcc3086ed9a1
dareen323 <dareenhiasat323@gmail.com>
2022-12-11 16:39:11 +0300

    edit profile

--- src/Components/Profile/UserInfo.jsx
+++ src/Components/Profile/UserInfo.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 import Uploder from '../Main-Component/Uploder'
 import SideBar from './SideBar'
 import Button from '../button/Button'
+import Modal from './portal/model'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UserInfo = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLocked, setIsLocked] = React.useState(true);
+  const [isLockedOpen, setIsLockedOpen] = React.useState(false);
   return (
     <SideBar>
        <div class="flex items-center  w-full">
@@ -46,7 +50,47 @@ const UserInfo = () => {
             
             </table>
             <div className='flex justify-end'>
-            <Button> Edit Profile</Button></div>
+            <Button  onClick={() => {
+          setIsOpen(true);
+        }}> Edit Profile</Button>
+        </div>
+        <Modal 
+        onClose={() => {
+          setIsOpen(false);
+        }}
+        open={isOpen}
+      >
+       <form class=" p-5 w-full">
+    <div class="mb-4">
+      <label class="block text-white-700 text-sm font-bold mb-2" for="username">
+        Name
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Name"/>
+    </div>
+    <div class="mb-4">
+      <label class="block text-white-700 text-sm font-bold mb-2" for="username">
+        Phone
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Phone"/>
+    </div>
+    <div class="mb-4">
+      <label class="block text-white-700 text-sm font-bold mb-2" for="username">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email"/>
+    </div>
+    <div class="mb-4">
+      <label class="block text-white-700 text-sm font-bold mb-2" for="username">
+        Age
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Age"/>
+    </div>
+    </form>
+        <p style={{ textAlign: "center" ,color:'black'}}>
+         
+         <Button>Edit</Button>
+        </p>
+      </Modal>
             {/* <div class="text-center my-3">
                 <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">Edit Profile</a>
             </div> */}
