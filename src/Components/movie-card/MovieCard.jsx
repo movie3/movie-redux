98bf994fd307e13be36e0cd2ce7d7ba3ef7e31de
khalledalkarmi <khalled_95@hotmail.com>
2022-12-11 17:07:13 +0300

    Update MovieCard.jsx
    add fav

--- src/Components/movie-card/MovieCard.jsx
+++ src/Components/movie-card/MovieCard.jsx
@@ -17,16 +17,28 @@ const MovieCard = (props) => {
 
   const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
 
-    return (
-        <Link to={link}>
-            <div className="movie-card" style={{backgroundImage: `url(${bg})`}}>
-                <Button>
-                    <i className="bx bx-play"></i>
-                </Button>
-            </div>
-            <h3>{item.title || item.name}</h3>
-        </Link>
-    );
+   return (
+    <>
+      <Link to={link}>
+        <div className="movie-card" style={{ backgroundImage: `url(${bg})` }}>
+          <Button>
+            <i className="bx bx-play"></i>
+          </Button>
+        </div>
+      </Link>
+
+      <div>
+        {" "}
+        <button className="bg-border hover:text-subMain transitions text-white px-4 py-3 rounded-full text-sm bg-opacity-30">
+          <FaHeart />
+        </button>
+      </div>
+      <div>
+        <h3>{item.title || item.name}</h3>
+      </div>
+    </>
+  );
+};
 }
 
 export default MovieCard;
