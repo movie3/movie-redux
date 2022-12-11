50c14f5f210e41ff51f09762896c20f70b566e78
omaralomarii <omar.alomarii911@gmail.com>
2022-12-11 15:12:36 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import { Footer, Header, Home } from "./Components";
 
-import 'swiper/swiper.min.css';
-import './assets/boxicons-2.0.7/css/boxicons.min.css';
-import './App.scss';
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import Profile from "./Components/Profile";
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="bg-main text-white ">
+      <div className="bg-black text-white ">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
