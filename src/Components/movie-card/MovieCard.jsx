3aee1faed50536bae207f44f275a3e350c26c889
khalledalkarmi <khalled_95@hotmail.com>
2022-12-11 22:23:32 +0300

    finish login signup

--- src/Components/movie-card/MovieCard.jsx
+++ src/Components/movie-card/MovieCard.jsx
@@ -9,15 +9,24 @@ import Button from "../button/Button";
 import { category } from "../../API/tmdbApi";
 import apiConfig from "../../API/apiConfig";
 import { FaHeart } from "react-icons/fa";
+import { useIsAuthenticated } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
 
 const MovieCard = (props) => {
+  const isAuth = useIsAuthenticated()
+  const navigate = useNavigate()
+
   const item = props.item;
+  // console.log(props.item);
+  const link = '/movie/' + item.id;
 
-    const link = '/movie/' + item.id;
-
+  const handleNavigate = () => {
+    return navigate('/login')
+  }
+  console.log(isAuth());
   const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
 
-   return (
+  return (
     <div>
       <Link to={link}>
         <div className="movie-card" style={{ backgroundImage: `url(${bg})` }}>
@@ -26,16 +35,29 @@ const MovieCard = (props) => {
           </Button>
         </div>
       </Link>
-
-      <div>
-        {" "}
-        <button className="bg-border hover:text-subMain transitions text-white px-4 py-3 rounded-full text-sm bg-opacity-30">
-          <FaHeart />
-        </button>
-      </div>
-      <div>
-        <h3>{item.title || item.name}</h3>
-      </div>
+      {isAuth() ? (
+        <>
+          <div>
+            <button className="bg-border hover:text-subMain transitions text-white px-4 py-3 rounded-full text-sm bg-opacity-30">
+              <FaHeart />
+            </button>
+          </div>
+          <div>
+            <h3>{item.title || item.name}</h3>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <button className="bg-border hover:text-subMain transitions text-white px-4 py-3 rounded-full text-sm bg-opacity-30" onClick={handleNavigate}>
+              <FaHeart />
+            </button>
+          </div>
+          <div>
+            <h3>{item.title || item.name}</h3>
+          </div>
+        </>
+      )}
     </div>
   );
 };
