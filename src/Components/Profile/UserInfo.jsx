9b246d4c06eadb7e25a31831764eeac6202f6b14
dareen323 <dareenhiasat323@gmail.com>
2022-12-11 11:54:16 +0300

    profile

--- src/Components/Profile/UserInfo.jsx
+++ src/Components/Profile/UserInfo.jsx
@@ -2,35 +2,56 @@ import { Input } from '@mantine/core'
 import React from 'react'
 import Uploder from '../Main-Component/Uploder'
 import SideBar from './SideBar'
+import Button from '../button/Button'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UserInfo = () => {
   return (
     <SideBar>
-      <div className="flex flex-col gap-6">
-        <h2 className="text-xl font-bold">Profile</h2>
-        <Uploder />
-        <Input
-          label="FullName"
-          placeholder="Full Name"
-          type="text"
-          bg={true}
-        />
-        <Input
-          label="Email"
-          placeholder="Your Email"
-          type="email"
-          bg={true}
-        />
-        <div className="flex gap-2 flex-wrap flex-col-reverse sm:flex-row justify-between items-center my-4">
-          <button className="bg-subMain font-medium transitions hover:bg-main border border-subMain text-white py-3 px-6 rounded w-full sm:w-auto">
-            Delete Account
-          </button>
-          <button className="bg-main font-medium transitions hover:bg-subMain border border-subMain text-white py-3 px-6 rounded w-full sm:w-auto">
-            Update Profile
-          </button>
+       <div class="flex items-center  w-full">
+
+<div class="w-full mx-8">
+    <div class="bg-light shadow-xl rounded-lg py-3">
+        <div class="photo-wrapper p-2">
+            <img class="w-32 h-32 rounded-full mx-auto" src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="John Doe"/>
         </div>
-      </div>
+        <div class="p-2">
+            <h1 class="text-center text-2xl text-white-900 font-medium leading-8 ">Joh Doe</h1>
+            <div class="text-center text-gray-400 text-xs font-semibold">
+                {/* <p>Web Developer</p> */}
+            </div>
+            <table class="text-xs my-3">
+                <tbody><tr>
+                    <td class="px-2 py-2 text-white-500 font-semibold text-xl">Address :</td>
+                    <td class="px-2 py-2 text-lg">Chatakpur-3, Dhangadhi Kailali</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-white-500 font-semibold text-xl">Phone :</td>
+                    <td class="px-2 py-2 text-lg">+977 9955221114</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-white-500 font-semibold text-xl">Email :</td>
+                    <td class="px-2 py-2 text-lg">john@exmaple.com</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-white-500 font-semibold text-xl">Gender :</td>
+                    <td class="px-2 py-2 text-lg">Famale</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-white-500 font-semibold text-xl">Age :</td>
+                    <td class="px-2 py-2 text-lg">22 </td>
+                </tr>
+            </tbody></table>
+            <Button> Edit Profile</Button>
+            {/* <div class="text-center my-3">
+                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">Edit Profile</a>
+            </div> */}
+
+        </div>
+    </div>
+</div>
+
+</div>
     </SideBar>  )
 }
 
