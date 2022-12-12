05e6230c3e0415f03edf111a01939652237a1798
aalawneh220 <ahmadalawneh329@gmail.com>
2022-12-12 11:50:42 +0300

    img

--- src/Components/iptv/Iptv.jsx
+++ src/Components/iptv/Iptv.jsx
@@ -15,7 +15,7 @@ const Iptv = () => {
           {names.map((name, url) => (
             <div className="border border-border p-1 hover:scale-95 transitions relative rounded overflow-hidden">
               <a href={name.url} className="w-full">
-                <img src={bg} className="w-full h-64 object-cover" />
+                <img src={name.img} className="w-full h-64 object-cover" />
               </a>
               <div className="absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-main bg-opacity-60 text-white px-4 py-3">
                 <h3 className="font-semibold truncate">{name.name}</h3>
